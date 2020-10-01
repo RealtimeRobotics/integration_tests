06ba5553eaa389182332fc14cf304f8aba6d8e30
Ryan Kelly <rkelly@rtr.ai>
2020-10-01 16:52:27 -0400

    setup and teardown

--- reg_test/reg_test_calibration_sim/test/CalibrationSimTest.cpp
+++ reg_test/reg_test_calibration_sim/test/CalibrationSimTest.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 
+#include <boost/filesystem.hpp>
 #include <ros/service.h>
 #include <QApplication>
 
@@ -12,6 +13,7 @@
 #include <gtest/gtest.h>
 
 #include "rtr_perc_rapidsense_ros/RapidSenseFrontEndProxy.hpp"
+#include "rtr_perc_rapidsense_ros/Record.hpp"
 #include "rtr_appliance/Appliance.hpp"
 #include "rtr_test_harness/RapidSenseTestHarnessServer.hpp"
 #include "rtr_test_harness/RapidSenseTestHelper.hpp"
@@ -32,28 +34,43 @@ int main(int argc, char** argv) {
   RUN_ALL_TESTS();
   
   server.Teardown();
-  bfs::remove_all("appliance_test");
+  bfs::remove_all("/tmp/appliance_test");
+  bfs::remove_all("/tmp/rapidsense_test");
 }
 
 class CalibrationTestFixture : public ::testing::Test {
 
   protected:
   ros::NodeHandle nh_;
-  RapidSenseFrontEndProxy proxy;
+  RapidSenseFrontEndProxy proxy_;
   RapidSenseTestHelper appliance_;
-  std::string robot_name, flange_frame, hub_name, project;
+  std::string decon_group_name, robot_name, flange_frame, hub_name, project, rapidsense_data;
   
   void SetUp() override {
+      nh_.param<std::string>("decon_group_name", decon_group_name, "ur3_calibration_test");
       nh_.param<std::string>("robot_name", robot_name, "ur3");
       nh_.param<std::string>("flange_frame", flange_frame, "ur3/flange");
       nh_.param<std::string>("hub_name", hub_name, "h_3");
       nh_.param<std::string>("project", project, "../../");
-
-      project = ros::package::getPath("reg_test_calibration_sim") + "/../../test_data/ur3_calibration_test/ur3-obstacle.zip";
-      //"/home/krishna/workspaces/master/src/rapidsense_test/reg_test/reg_test_calibration_sim/../../test_data/ur3_calibration_test/ur3-obstacle.zip"; //ros::package::getPath("reg_test_calibration_sim") +
-      RTR_INFO("Value of project={}", project);
+      nh_.param<std::string>("rapidsense_data", rapidsense_data, "../../");
       
-      appliance_.CreateAndSetupProject("ur3_calibration_test", project);
+      project = ros::package::getPath("reg_test_calibration_sim") + "/../../test_data/ur3_calibration_test/ur3-obstacle.zip";
+      rapidsense_data = ros::package::getPath("reg_test_calibration_sim") + "/../../test_data/ur3_calibration_test/rapidsense_data/";
+      RTR_INFO("Value of project={}", project);
+      RTR_INFO("Value of rapidsense_data={}", rapidsense_data);
+
+      
+      std::string rapidsense_state_directory;
+      if (!proxy_.GetStateDirectory(rapidsense_state_directory)) {
+        RTR_ERROR("Unable to get state directory from rapidsense");
+      }
+      
+      appliance_.CreateAndSetupProject(decon_group_name, project);
+
+      std::string rapidsense_data_directory = fmt::format("{}/{}/",rapidsense_state_directory, decon_group_name);
+      CopyFolder(rapidsense_data, rapidsense_data_directory);
+
+
       std_srvs::Trigger trg;
       CallRosService<std_srvs::Trigger>(nh_, trg, "/restart_sim");
 
@@ -66,33 +83,32 @@ class CalibrationTestFixture : public ::testing::Test {
   }
     
 public:
-    CalibrationTestFixture() : nh_(""), appliance_(nh_), proxy(RapidSenseFrontEndProxy::ProxyHost::RAPIDSENSE_GUI) { RTR_ERROR("CONSTRUCTION DEFAULT");}
-    CalibrationTestFixture(ros::NodeHandle& nh) : nh_(nh), appliance_(nh_), proxy(RapidSenseFrontEndProxy::ProxyHost::RAPIDSENSE_GUI) {
-      RTR_ERROR("CONSTRUCTION SUPPLIED");
-      // Start our proxy as the Rapidsense gui so the server doesn't automatically try to transition
+    CalibrationTestFixture() : nh_(""), appliance_(nh_), proxy_(RapidSenseFrontEndProxy::ProxyHost::RAPIDSENSE_GUI) { }
+    CalibrationTestFixture(ros::NodeHandle& nh) : nh_(nh), appliance_(nh_), proxy_(RapidSenseFrontEndProxy::ProxyHost::RAPIDSENSE_GUI) {
+      // Start our proxy_ as the Rapidsense gui so the server doesn't automatically try to transition
       // while we are testing
     }
 };
 
 
 TEST_F(CalibrationTestFixture, VerifyCailbrationWorkflowWithPreviousLoc) { 
-  proxy.RefreshAllRobots();
+  proxy_.RefreshAllRobots();
   
-  EXPECT_EQ(proxy.GetHealth().input_mode, RapidSenseInputMode::SIMULATION);
-  if (proxy.GetState() != RapidSenseState::CONFIGURE) {
-    EXPECT_TRUE(proxy.SetConfigureMode());
+  EXPECT_EQ(proxy_.GetHealth().input_mode, RapidSenseInputMode::SIMULATION);
+  if (proxy_.GetState() != RapidSenseState::CONFIGURE) {
+    EXPECT_TRUE(proxy_.SetConfigureMode());
   }
 
   // Configure the Robot for Calibration
   // Set Active Observer (arg)
   std::string active_observer = robot_name;
-  EXPECT_TRUE(proxy.SetActiveRobotObserver(active_observer));
+  EXPECT_TRUE(proxy_.SetActiveRobotObserver(active_observer));
   
   // Set Flange Frame (arg)
-  EXPECT_TRUE(proxy.SetFlangeFrame(flange_frame));
+  EXPECT_TRUE(proxy_.SetFlangeFrame(flange_frame));
   
   // Attach Fiducial
-  EXPECT_TRUE(proxy.AttachFiducialToActiveObserver());
+  EXPECT_TRUE(proxy_.AttachFiducialToActiveObserver());
   
   // Get Hubs (arg)
   const std::string hub_config_topic = "/GetHubConfig";
@@ -113,21 +129,21 @@ TEST_F(CalibrationTestFixture, VerifyCailbrationWorkflowWithPreviousLoc) {
   teleport_srv.request.joint_config = joint_config.GetData();
   EXPECT_TRUE(teleport_service.call(teleport_srv) && teleport_srv.response.is_success);
   
-  std::set<std::string> cameras_set = proxy.GetConnectedSensors();
+  std::set<std::string> cameras_set = proxy_.GetConnectedSensors();
   std::vector<std::string> uids(cameras_set.begin(), cameras_set.end());
 
-  AllSensorData pre_calibration_data = proxy.GetCalibration();
+  AllSensorData pre_calibration_data = proxy_.GetCalibration();
   for (const std::string& uid : uids) {
-    EXPECT_TRUE(proxy.EnableCalibrator(uid));
+    EXPECT_TRUE(proxy_.EnableCalibrator(uid));
   }
   std::vector<std::string> failed_uids;
-  EXPECT_TRUE(proxy.UseExistingCalibrationForInitGuess(uids, failed_uids));
+  EXPECT_TRUE(proxy_.UseExistingCalibrationForInitGuess(uids, failed_uids));
   
   EXPECT_TRUE(failed_uids.empty());
-  EXPECT_TRUE(proxy.SetCalibrateMode());
-  EXPECT_TRUE(proxy.CalibrateAllCameras());
-  EXPECT_TRUE(proxy.SetConfigureMode());
-  AllSensorData post_calibration_data = proxy.GetCalibration();
+  EXPECT_TRUE(proxy_.SetCalibrateMode());
+  EXPECT_TRUE(proxy_.CalibrateAllCameras());
+  EXPECT_TRUE(proxy_.SetConfigureMode());
+  AllSensorData post_calibration_data = proxy_.GetCalibration();
   EXPECT_TRUE(pre_calibration_data.FuzzyEquals(post_calibration_data, .025f));
   
   RTR_INFO("Calibration Test Successful!");
