dd397c7863f2450f05d18e60993d9b593218f7d6
Krishna Gopalakrishnan <krishna@rtr.ai>
2020-10-21 16:49:06 -0400

    update change to master. change this to be a sourceable function (#11)

--- scripts/run_perc_tests.sh
+++ scripts/run_perc_tests.sh
@@ -1,52 +1,72 @@
 #!/bin/sh
 
-usage() {
-    cat <<EOF
-Usage: $0 [OPTION]...
-Run perception integration and regtests.
+################################################################################
+#disable_flakes=0
+#while getopts ":hd:" opt; do
+#    case "${opt}" in
+#        h)
+#            usage
+#            exit 0
+#            ;;
+#        d)
+#            disable_flakes=1
+#            ;;
+#        :) die "missing argument: -${OPTARG}" ;;
+#        \?) die "bad option: -${OPTARG}" ;;
+#    esac
+#done
+#shift "$((${OPTIND} - 1))"
 
-Options:
-    -h    print usage and exit
-    -d    disable time sensitives tests
-
-EOF
+get_test_packages() {
+    local packages1="$(catkin list | grep 'unit_test\|integ_test\|reg_test' | sed 's/- / /g' | tr -d '\n')"
+    local  __resultvar=$1
+    eval $__resultvar="'$packages1'"
 }
 
 ################################################################################
+get_build_dir() {
+    local  __resultvar=$1
+    local build_dir1="$(catkin locate -b)"
+    eval $__resultvar="'$build_dir1/'"
+}
 
-disable_flakes=0
+# pass 0 to disable_flakes and 1 to run them
+run_perc_tests() {
+    local disable_flakes=$1
+    get_test_packages packages
+    echo "Running tests from the following packages: ${packages}"
+    if [ "${disable_flakes}" -eq 1 ]; then
+        env DISABLE_RS_FLAKYTESTS=1 catkin run_tests --no-deps ${packages}
+    else
+        env -u DISABLE_RS_FLAKYTESTS catkin run_tests --no-deps ${packages}
+    fi
+}
 
-while getopts ":hd:" opt; do
-    case "${opt}" in
-        h)
-            usage
-            exit 0
-            ;;
-        d)
-            disable_flakes=1
-            ;;
-        :) die "missing argument: -${OPTARG}" ;;
-        \?) die "bad option: -${OPTARG}" ;;
-    esac
-done
-shift "$((${OPTIND} - 1))"
+show_perc_results() {
+    get_test_packages packages
+    get_build_dir build_dir
+    echo "showing tests from the following packages: ${packages} and ${build_dir}"
+    echo "\n\nTEST RESULTS\n------------------------------------------------------------------"
+    local results_dirs="${build_dir}$(echo ${packages} | sed "s| | ${build_dir}|g")"
+    for result_dir in ${results_dirs}
+    do
+        echo $result_dir
+        catkin_test_results --all --verbose ${result_dir}
+        echo ""
+    done
+}
 
-readonly packages="$(catkin list | grep 'unit_test\|integ_test\|reg_test' | sed 's/- / /g' | tr -d '\n')"
-echo "Running tests from the following packages: ${packages}"
+show_perc_results_summary() {
+    get_test_packages packages
+    get_build_dir build_dir
+    local results_dirs="${build_dir}$(echo ${packages} | sed "s| | ${build_dir}|g")"
+    echo "TEST RESULT SUMMARY"
+    echo "-------------------"
+    for result_dir in ${results_dirs}
+    do
+        echo $result_dir
+        catkin_test_results --all ${result_dir}
+        echo ""
+    done
+}
 
-if [ "${disable_flakes}" -eq 1 ]; then
-    env DISABLE_RS_FLAKYTESTS=1 catkin run_tests --no-deps ${packages}
-else
-    env -u DISABLE_RS_FLAKYTESTS catkin run_tests --no-deps ${packages}
-fi
-
-echo "\n\nTEST RESULTS\n------------------------------------------------------------------"
-readonly build_dir="$(catkin locate)/build/"
-readonly results_dirs="${build_dir}$(echo ${packages} | sed "s| | ${build_dir}|g")"
-for result_dir in ${results_dirs}
-do
-    catkin_test_results --all --verbose ${result_dir}
-    echo ""
-done
-
-exit 1
